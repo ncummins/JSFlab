cbdadec7055c707f2b6d0493bf962d43c2a73100
ncummins <ncummins@L11718038612.wctc.msft>
2013-10-16 21:02:48 -0500

    first commit

---
+++ src/java/controller/HelloBean.java
@@ -0,0 +1,35 @@
+
+package controller;
+
+/**
+ *
+ * @author ncummins
+ */
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+ 
+@Named
+@SessionScoped
+public class HelloBean implements Serializable {
+ 
+	private static final long serialVersionUID = 1L;
+ 
+	private String name;
+ 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+        public String getSayWelcome(){
+	   //check if null?
+	   if("".equals(name) || name ==null){
+		return "";
+	   }else{
+		return "Ajax message : Welcome " + name;
+	   }
+	}
+}
